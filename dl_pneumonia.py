1dbba0c2340febb674bdfea16844e5416d16466d
rituja-pardhi <62795066+rituja-pardhi@users.noreply.github.com>
2020-03-28 18:34:30 +0530

    Add files via upload

---
+++ dl_pneumonia.py
@@ -0,0 +1,198 @@
+import os
+import numpy as np
+import pandas as pd 
+import random
+import cv2
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+import keras.backend as K
+from keras.models import Model, Sequential
+from keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, SeparableConv2D, MaxPool2D, LeakyReLU, Activation
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+import tensorflow as tf
+
+seed = 232
+np.random.seed(seed)
+tf.set_random_seed(seed)
+input_path = 'C:/Users/rituj/Desktop/project/chest_xray/'
+
+fig, ax = plt.subplots(2, 2, figsize=(15, 7))
+ax = ax.ravel()
+plt.tight_layout()
+
+for i, _set in enumerate(['train', 'test']):
+    set_path = input_path+_set
+    ax[i].imshow(plt.imread(set_path+'/NORMAL/'+os.listdir(set_path+'/NORMAL')[0]), cmap='gray')
+    ax[i].set_title('Set: {}, Condition: Normal'.format(_set))
+    ax[i+2].imshow(plt.imread(set_path+'/PNEUMONIA/'+os.listdir(set_path+'/PNEUMONIA')[0]), cmap='gray')
+    ax[i+2].set_title('Set: {}, Condition: Pneumonia'.format(_set))
+for _set in ['train', 'test']:
+    n_normal = len(os.listdir(input_path + _set + '/NORMAL'))
+    n_infect = len(os.listdir(input_path + _set + '/PNEUMONIA'))
+    print('Set: {}, normal images: {}, pneumonia images: {}'.format(_set, n_normal, n_infect))
+def process_data(img_dims, batch_size):
+    train_datagen = ImageDataGenerator(rescale=1./255, zoom_range=0.3, vertical_flip=True)
+    test_val_datagen = ImageDataGenerator(rescale=1./255)
+    
+    train_gen = train_datagen.flow_from_directory(
+    directory=input_path+'train', 
+    target_size=(img_dims, img_dims), 
+    batch_size=batch_size, 
+    class_mode='binary', 
+    shuffle=True)
+
+    test_gen = test_val_datagen.flow_from_directory(
+    directory=input_path+'test', 
+    target_size=(img_dims, img_dims), 
+    batch_size=batch_size, 
+    class_mode='binary', 
+    shuffle=True)
+    
+    test_data = []
+    test_labels = []
+
+    for cond in ['/NORMAL/', '/PNEUMONIA/']:
+        for img in (os.listdir(input_path + 'test' + cond)):
+            img = plt.imread(input_path+'test'+cond+img)
+            img = cv2.resize(img, (img_dims, img_dims))
+            img = np.dstack([img, img, img])
+            img = img.astype('float32') / 255
+            if cond=='/NORMAL/':
+                label = 0
+            elif cond=='/PNEUMONIA/':
+                label = 1
+            test_data.append(img)
+            test_labels.append(label)
+        
+    test_data = np.array(test_data)
+    test_labels = np.array(test_labels)
+    
+    return train_gen, test_gen, test_data, test_labels
+
+img_dims = 150
+epochs = 10
+batch_size = 32
+
+train_gen, test_gen, test_data, test_labels = process_data(img_dims, batch_size)
+
+inputs = Input(shape=(img_dims, img_dims, 3))
+
+x = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+x = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+
+x = SeparableConv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+
+x = SeparableConv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+
+x = SeparableConv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+x = Dropout(rate=0.2)(x)
+
+x = SeparableConv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+x = Dropout(rate=0.2)(x)
+
+x = Flatten()(x)
+x = Dense(units=512, activation='relu')(x)
+x = Dropout(rate=0.7)(x)
+x = Dense(units=128, activation='relu')(x)
+x = Dropout(rate=0.5)(x)
+x = Dense(units=64, activation='relu')(x)
+x = Dropout(rate=0.3)(x)
+
+output = Dense(units=1, activation='sigmoid')(x)
+
+model = Model(inputs=inputs, outputs=output)
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+checkpoint = ModelCheckpoint(filepath='best_weights.hdf5', save_best_only=True, save_weights_only=True)
+lr_reduce = ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=2, verbose=2, mode='max')
+early_stop = EarlyStopping(monitor='val_loss', min_delta=0.1, patience=1, mode='min')
+
+hist = model.fit_generator(
+           train_gen, steps_per_epoch=train_gen.samples // batch_size, 
+           epochs=epochs, validation_data=test_gen, 
+           validation_steps=test_gen.samples // batch_size, callbacks=[checkpoint, lr_reduce])
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+ax = ax.ravel()
+
+for i, met in enumerate(['acc', 'loss']):
+    ax[i].plot(hist.history[met])
+    ax[i].plot(hist.history['val_' + met])
+    ax[i].set_title('Model {}'.format(met))
+    ax[i].set_xlabel('epochs')
+    ax[i].set_ylabel(met)
+    ax[i].legend(['train', 'val'])
+    
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+preds = model.predict(test_data)
+
+acc = accuracy_score(test_labels, np.round(preds))*100
+cm = confusion_matrix(test_labels, np.round(preds))
+tn, fp, fn, tp = cm.ravel()
+
+print('CONFUSION MATRIX ------------------')
+print(cm)
+
+print('\nTEST METRICS ----------------------')
+precision = tp/(tp+fp)*100
+recall = tp/(tp+fn)*100
+print('Accuracy: {}%'.format(acc))
+print('Precision: {}%'.format(precision))
+print('Recall: {}%'.format(recall))
+print('F1-score: {}'.format(2*precision*recall/(precision+recall)))
+
+print('\nTRAIN METRIC ----------------------')
+print('Train acc: {}'.format(np.round((hist.history['acc'][-1])*100, 2)))
+
+from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
+def load_image(img_path, show=False):
+
+    img = load_img(img_path, target_size=(150, 150))
+    img_tensor = img_to_array(img)                   
+    img_tensor = np.expand_dims(img_tensor, axis=0)        
+    img_tensor /= 255.                                      
+
+    if show:
+        plt.imshow(img_tensor[0])                           
+        plt.axis('off')
+        plt.show()
+
+    return img_tensor
+
+
+if __name__ == "__main__":
+
+    img_path = 'C:/Users/rituj/Desktop/project/chest_xray_try/test/PNEUMONIA/bacterial/person78_bacteria_382.jpeg'    
+    
+    # load a single image
+    new_image = load_image(img_path)
+
+    # check prediction
+    pred = model.predict(new_image)
+    
+    print(pred)
+    if pred>=0.5:
+        print("pneumonia")
+    else: 
+        print("normal")
+        
+model.save('pneumonia.h5')
